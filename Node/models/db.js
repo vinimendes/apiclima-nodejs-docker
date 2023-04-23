55d1eb6935f769e0ebbbdc2732a8873dfa2aa71b
Vinicius Medeiros <vini9797@hotmail.com>
2023-04-23 16:16:53 -0300

    correcao para casos de CRASH

--- Node/models/db.js
+++ Node/models/db.js
@@ -2,4 +2,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('postgres://postgres:passwdtechstorm@postgres:5432/techstorm');
 
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexao realizada com sucesso!")
+}).catch(function(){
+    console.log("erro")
+})
+
 module.exports = sequelize;
